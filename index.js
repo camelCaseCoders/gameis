0dfdedc1901b67400920e322b62b440b53f439f9
John Rapp Farnes <johnfarnes@hotmail.com>
2014-02-04 20:33:26 +0100

    Added mongojs

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express'),
 	app = express(),
-	server = require('http').createServer(app);
+	server = require('http').createServer(app),
+	db = require("mongojs").connect(databaseUrl, collections);
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client'))
+.use(express.json());
 
 server.listen(8080);
 console.log('SERVER UP AND RUNNING');
 
-var highScores = [];
+var highscores = [];
 
+app.get('/highscores', function (req, res, next) {
+	res.json(highscores);
+	highscores.push(Math.floor(Math.random() * 100));
+});
+
+app.post('/newscore', function (req, res, next) {
+	highscores.add(req.body);
+	res.json(true);
+});
