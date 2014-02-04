77494757902425a485bea30084524a1f2fc64eb6
John Rapp Farnes <johnfarnes@hotmail.com>
2014-02-04 19:59:06 +0100

    Changed server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express')
-	, app = express()
-	, server = require('http').createServer(app);
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app);
 
 app.use(express.static(__dirname + '/client'));
 
 server.listen(8080);
 console.log('SERVER UP AND RUNNING');
+
+var highScores = [];
+
