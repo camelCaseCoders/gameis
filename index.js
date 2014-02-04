74438446339a30f20932d32308999c3190e05418
John Rapp Farnes <johnfarnes@hotmail.com>
2014-02-04 21:54:27 +0100

    Some mongodb experimentation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	db = require("mongojs").connect('', ["users", "reports"]);
+	db = require("mongojs").connect('highscoredb', ['highscores']);
 
 app.use(express.static(__dirname + '/client'))
 .use(express.json());
@@ -9,14 +9,20 @@ app.use(express.static(__dirname + '/client'))
 server.listen(8080);
 console.log('SERVER UP AND RUNNING');
 
-var highscores = [];
-
 app.get('/highscores', function (req, res, next) {
-	res.json(highscores);
-	highscores.push(Math.floor(Math.random() * 100));
+	var scores = [];
+	db.highscores.find().forEach(function(err, doc) {
+		if(doc) {
+		    // doc is a document in the collection
+		    scores.push(doc)
+		} else {
+	        // we visited all docs in the collection
+			res.json(scores);
+	        return;
+		}
+	});
 });
 
 app.post('/newscore', function (req, res, next) {
-	highscores.add(req.body);
 	res.json(true);
 });
