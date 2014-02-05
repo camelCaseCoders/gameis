4f8664f6b7fcf17bbff6826de5cb0ef5c72feb45
John Rapp Farnes <johnfarnes@hotmail.com>
2014-02-05 17:28:37 +0100

    Changed to mongoose

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	db = require("mongojs").connect('highscoredb', ['highscores']);
+	mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/highscores');
 
 app.use(express.static(__dirname + '/client'))
 .use(express.json());
@@ -9,20 +11,31 @@ app.use(express.static(__dirname + '/client'))
 server.listen(8080);
 console.log('SERVER UP AND RUNNING');
 
-app.get('/highscores', function (req, res, next) {
-	var scores = [];
-	db.highscores.find().forEach(function(err, doc) {
-		if(doc) {
-		    // doc is a document in the collection
-		    scores.push(doc)
-		} else {
-	        // we visited all docs in the collection
-			res.json(scores);
-	        return;
-		}
+
+var Schema = mongoose.Schema,
+	Score;
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	var scoreSchema = new Schema({
+		name: {type: String, default: 'Anonymous'},
+		score: {type: Number}
 	});
+	Score = mongoose.model('Score', scoreSchema);
 });
 
-app.post('/newscore', function (req, res, next) {
-	res.json(true);
+app.get('/highscores', function (req, res, next) {
+	Score.find(function (err, scores) {
+	  	if (err) console.log(err);
+	  	res.json(scores);
+	})
+});
+
+app.get('/newscore', function (req, res, next) {
+	Score.create({name: 'John', score: Math.floor(Math.random * 100)}, function(err, score) {
+		if (err) return console.log(err);
+		console.log(score);
+	});
 });
