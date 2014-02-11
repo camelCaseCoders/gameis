f4f09ef1c3e441ff2e96a12f86628b8daf159fc7
John Rapp Farnes <johnfarnes@hotmail.com>
2014-02-11 22:17:30 +0100

    Seriously nothing

--- client/lib/blink.js
+++ client/lib/blink.js
@@ -1,22 +1,22 @@
 angular.module('blink', [])
 .directive('blink', function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        transclude: true,
-        template: '<span ng-transclude></span>',
-        scope: {speed: '@'},
-        link: function (scope, element, attrs) {
-            var visible = true,
-                speed = scope.speed || 100;
-            
-            var blink = function() {
-                if(visible) element.css('visibility', 'hidden');
-                else element.css('visibility', 'visible');
-                visible = !visible;
-                window.setTimeout(blink, speed);   
-            }
-            blink();
-        }
-    }
+	return {
+		restrict: 'E',
+		replace: true,
+		transclude: true,
+		template: '<span ng-transclude></span>',
+		scope: {speed: '@'},
+		link: function (scope, element, attrs) {
+			var visible = true,
+				speed = scope.speed || 100;
+			
+			var blink = function() {
+				if(visible) element.css('visibility', 'hidden');
+				else element.css('visibility', 'visible');
+				visible = !visible;
+				window.setTimeout(blink, speed);    
+			}
+			blink();
+		}
+	}
 });
